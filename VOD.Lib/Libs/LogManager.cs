c34599806a246a3d3dcc39ea0626848544ba99e3
michael-eddy <michael19950415@outlook.com>
2020-05-05 15:33:52 +0800

    new file:   VOD.Lib/Libs/LogManager.cs 	modified:   VOD.Lib/Bilibili/ApiClient.cs 	modified:   VOD.Lib/HttpClientEx.cs 	modified:   VOD.Lib/Models/DanmakuProtocol.cs 	modified:   VOD.Lib/Models/EventModel.cs 	modified:   VOD.Lib/MusicClient.cs 	modified:   VOD.Lib/User.cs 	modified:   VOD.Lib/Utils.cs 	modified:   VOD.Lib/VOD.Lib.csproj 	modified:   VOD.Lib/packages.config 	modified:   VOD.Wpf/App.xaml.cs 	modified:   VOD.Wpf/Danmaku.cs 	modified:   VOD.Wpf/Dialogs/Setting.xaml 	modified:   VOD.Wpf/Dialogs/Setting.xaml.cs 	modified:   VOD.Wpf/MainWindow.xaml.cs

---
+++ VOD.Lib/Libs/LogManager.cs
@@ -0,0 +1,45 @@
+﻿using Serilog;
+using Serilog.Core;
+using System;
+using System.IO;
+
+namespace VOD.Lib.Libs
+{
+    public sealed class LogManager
+    {
+        public static LogManager Instance { get; } = new LogManager();
+        private Logger Logger { get; }
+        private LogManager()
+        {
+            var LogFile = Path.Combine(Environment.CurrentDirectory, "bilibili_log", "log-.txt");
+            Logger = new LoggerConfiguration().MinimumLevel.Debug()
+                .WriteTo.File(LogFile, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+        public void LogError(string messageTemplate, Exception exception)
+        {
+            try
+            {
+                if (exception.IsNotEmpty())
+                    Logger.Error(exception, messageTemplate);
+            }
+            catch { }
+        }
+        public void LogInfo(string messageTemplate)
+        {
+            try
+            {
+                Logger.Information(messageTemplate);
+            }
+            catch { }
+        }
+        public void LogInfo(string messageTemplate, params object[] propertyValues)
+        {
+            try
+            {
+                Logger.Information(messageTemplate, propertyValues);
+            }
+            catch { }
+        }
+    }
+}
